1a003f7e64b1b95901cfbf9649af0b037b3d3a97
Parvez141300 <parvezhossain744471@gmail.com>
2026-06-21 17:34:47 +0600

    improve the auth.ts file added baseUrl and secret

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -2,9 +2,12 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 import { Role } from "../../generated/prisma/enums";
+import { envVars } from "../utils/env";
 // If your Prisma file is located elsewhere, you can change the path
 
 export const auth = betterAuth({
+    baseURL: envVars.BETTER_AUTH_URL,
+    secret: envVars.BETTER_AUTH_SECRET,
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
