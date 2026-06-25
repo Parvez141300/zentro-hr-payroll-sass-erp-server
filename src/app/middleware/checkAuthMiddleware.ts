4a67946c1cd75cae68476f3bc4a70f05d629ec00
Parvez141300 <parvezhossain744471@gmail.com>
2026-06-25 19:37:58 +0600

    improve the check auth middleware

--- src/app/middleware/checkAuthMiddleware.ts
+++ src/app/middleware/checkAuthMiddleware.ts
@@ -7,85 +7,89 @@ import { Role } from "../../generated/prisma/enums";
 
 export const checkAuthMiddleware = (...authRoles: Role[]) => {
     return async (req: Request, res: Response, next: NextFunction) => {
-        const sessionToken = cookieUtils.getCookie(req, "accessToken");
+        try {
+            const sessionToken = cookieUtils.getCookie(req, "accessToken");
 
-        if (!sessionToken) {
-            throw new Error("Unauthorized access! No session token found.");
-        }
+            if (!sessionToken) {
+                throw new Error("Unauthorized access! No session token found.");
+            }
 
-        if (sessionToken) {
-            const isExistSessionToken = await prisma.session.findUnique({
-                where: {
-                    token: sessionToken,
-                    expiresAt: {
-                        gt: new Date(),
+            if (sessionToken) {
+                const isExistSessionToken = await prisma.session.findUnique({
+                    where: {
+                        token: sessionToken,
+                        expiresAt: {
+                            gt: new Date(),
+                        },
                     },
-                },
-                include: {
-                    user: true,
-                }
-            });
+                    include: {
+                        user: true,
+                    }
+                });
 
-            if (isExistSessionToken && isExistSessionToken.user) {
-                const user = isExistSessionToken.user;
-                console.log("user data from check auth middleware", user);
+                if (isExistSessionToken && isExistSessionToken.user) {
+                    const user = isExistSessionToken.user;
+                    console.log("user data from check auth middleware", user);
 
-                const now = new Date();
-                const expiresAt = new Date(isExistSessionToken.expiresAt);
-                const createdAt = new Date(isExistSessionToken.createdAt);
+                    const now = new Date();
+                    const expiresAt = new Date(isExistSessionToken.expiresAt);
+                    const createdAt = new Date(isExistSessionToken.createdAt);
 
-                const sessionLifeTime = expiresAt.getTime() - createdAt.getTime();
-                const remainingTime = expiresAt.getTime() - now.getTime();
+                    const sessionLifeTime = expiresAt.getTime() - createdAt.getTime();
+                    const remainingTime = expiresAt.getTime() - now.getTime();
 
-                const remainingTimePercentage = (remainingTime / sessionLifeTime) * 100;
+                    const remainingTimePercentage = (remainingTime / sessionLifeTime) * 100;
 
-                console.log("remaining time percentage", remainingTimePercentage);
+                    console.log("remaining time percentage", remainingTimePercentage);
 
-                if (remainingTimePercentage < 20) {
-                    res.set({
-                        "X-Session-Refresh": "true",
-                        "X-Session-Expires-At": expiresAt.toISOString(),
-                        "X-Time-Remaining": remainingTime.toString(),
-                    });
+                    if (remainingTimePercentage < 20) {
+                        res.set({
+                            "X-Session-Refresh": "true",
+                            "X-Session-Expires-At": expiresAt.toISOString(),
+                            "X-Time-Remaining": remainingTime.toString(),
+                        });
 
-                    console.log('sessin is expiring soon');
-                }
+                        console.log('sessin is expiring soon');
+                    }
 
-                if (!user.isActive) {
-                    throw new Error("Unauthorized access! User is inactive.");
-                }
+                    if (!user.isActive) {
+                        throw new Error("Unauthorized access! User is inactive.");
+                    }
 
-                if (user.isDeleted) {
-                    throw new Error("Unauthorized access! User is deleted.");
+                    if (user.isDeleted) {
+                        throw new Error("Unauthorized access! User is deleted.");
+                    }
                 }
             }
-        }
 
-        // access token check
-        const accessToken = cookieUtils.getCookie(req, "accessToken");
-        if (!accessToken) {
-            throw new Error("Unauthorized access! No access token found.");
-        }
+            // access token check
+            const accessToken = cookieUtils.getCookie(req, "accessToken");
+            if (!accessToken) {
+                throw new Error("Unauthorized access! No access token found.");
+            }
 
-        const verifyToken = jwtUtils.verifyToken(accessToken, envVars.JWT_TOKEN_SECRET);
-        if (!verifyToken.success) {
-            throw new Error("Unauthorized access! Invalid access token.");
-        }
+            const verifyToken = jwtUtils.verifyToken(accessToken, envVars.JWT_TOKEN_SECRET);
+            if (!verifyToken.success) {
+                throw new Error("Unauthorized access! Invalid access token.");
+            }
 
-        req.user = {
-            companId: verifyToken.data?.companyId,
-            userId: verifyToken.data?.userId,
-            name: verifyToken.data?.name,
-            email: verifyToken.data?.email,
-            role: verifyToken.data?.role,
-            isActive: verifyToken.data?.isActive,
-            isDeleted: verifyToken.data?.isDeleted
-        }
+            req.user = {
+                companId: verifyToken.data?.companyId,
+                userId: verifyToken.data?.userId,
+                name: verifyToken.data?.name,
+                email: verifyToken.data?.email,
+                role: verifyToken.data?.role,
+                isActive: verifyToken.data?.isActive,
+                isDeleted: verifyToken.data?.isDeleted
+            }
 
-        if (authRoles.length > 0 && !authRoles.includes(verifyToken.data?.role as Role)) {
-            throw new Error("Unauthorized access! Invalid user role.");
-        }
+            if (authRoles.length > 0 && !authRoles.includes(verifyToken.data?.role as Role)) {
+                throw new Error("Unauthorized access! Invalid user role.");
+            }
 
-        next();
+            next();
+        } catch (error) {
+            next(error);
+        }
     }
 }
