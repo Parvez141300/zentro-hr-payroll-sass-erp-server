ffec9b1afd29ae1ac96bfac56348506d4f6bf5f5
Parvez141300 <parvezhossain744471@gmail.com>
2026-06-27 12:42:49 +0600

    improve the hr and accoutant creation route with a check auth middleware

--- src/app/module/user/user.route.ts
+++ src/app/module/user/user.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import { checkAuthMiddleware } from "../../middleware/checkAuthMiddleware";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
 
-router.post("/create-company-hr", userController.createCompanyHr);
-router.post("/create-company-accountant", userController.createCompanyAccountant);
+router.post("/create-company-hr", checkAuthMiddleware(Role.Super_ADMIN), userController.createCompanyHr);
+router.post("/create-company-accountant", checkAuthMiddleware(Role.Super_ADMIN), userController.createCompanyAccountant);
 
 export const userRoute = router;
