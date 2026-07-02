44858aa5a4f9458f94a2d3c2ca6f99d34ee095ce
Parvez141300 <parvezhossain744471@gmail.com>
2026-07-02 18:25:23 +0600

    imporve the dashboard stats api route

--- src/app/module/stats/stats.route.ts
+++ src/app/module/stats/stats.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { statsController } from "./statscontroller";
+import { checkAuthMiddleware } from "../../middleware/checkAuthMiddleware";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
 
-router.get("/dashboard/stats", statsController.getDashboardStats)
+router.get("/dashboard/stats", checkAuthMiddleware(Role.Super_ADMIN, Role.HR_MANAGER, Role.DEPARTMENT_HEAD, Role.EMPLOYEE, Role.ACCOUNTANT), statsController.getDashboardStats)
 
 export const statsRoute = router;
