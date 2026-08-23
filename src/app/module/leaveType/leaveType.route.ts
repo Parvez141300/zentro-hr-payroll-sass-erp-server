89254b35269ed55498abdf7813d0a3217ee21c70
Parvez141300 <parvezhossain744471@gmail.com>
2026-08-23 11:27:48 +0600

    resovle the bugs in the leave types route added the check auth middleware in the route

--- src/app/module/leaveType/leaveType.route.ts
+++ src/app/module/leaveType/leaveType.route.ts
@@ -1,11 +1,29 @@
 import { Router } from "express";
 import { leaveTypeController } from "./leaveType.controller";
+import { checkAuthMiddleware } from "../../middleware/checkAuthMiddleware";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
 
-router.get("/", leaveTypeController.getAllOrQueryLeaveTypes);
-router.post("/", leaveTypeController.createLeaveType);
-router.patch("/:id", leaveTypeController.updateLeaveType);
-router.delete("/:id", leaveTypeController.deleteLeaveType);
+router.get(
+    "/",
+    checkAuthMiddleware(Role.Super_ADMIN, Role.HR_MANAGER),
+    leaveTypeController.getAllOrQueryLeaveTypes
+);
+router.post(
+    "/", 
+    checkAuthMiddleware(Role.Super_ADMIN, Role.HR_MANAGER), 
+    leaveTypeController.createLeaveType
+);
+router.patch(
+    "/:id", 
+    checkAuthMiddleware(Role.Super_ADMIN, Role.HR_MANAGER),
+    leaveTypeController.updateLeaveType
+);
+router.delete(
+    "/:id", 
+    checkAuthMiddleware(Role.Super_ADMIN, Role.HR_MANAGER),
+    leaveTypeController.deleteLeaveType
+);
 
 export const leaveTypeRoute = router;
