3eb822b1903cb863cdac4096bad9e3e1aec95a06
Parvez141300 <parvezhossain744471@gmail.com>
2026-09-01 20:51:41 +0600

    added a checkauth middleware in the payroll get all or query route

--- src/app/module/payroll/payroll.route.ts
+++ src/app/module/payroll/payroll.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { payrollController } from "./payroll.controller";
+import { checkAuthMiddleware } from "../../middleware/checkAuthMiddleware";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
 
-router.post("/", payrollController.generatePayroll);
+router.post(
+    "/",
+    checkAuthMiddleware(Role.Super_ADMIN, Role.ACCOUNTANT),
+    payrollController.generatePayroll
+);
 router.get("/", payrollController.getAllOrQueryPayrolls);
 router.patch("/:id", payrollController.updatePayrollInDB);
 router.get("/:id/payslip", payrollController.getPayslipData);
